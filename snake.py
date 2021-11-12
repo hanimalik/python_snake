35888fe2c1f46fa726885817de1203d5d2ae51a3
hanimalik <malik2hx@dukes.jmu.edu>
2021-11-12 13:54:48 -0500

    update

---
+++ snake.py
@@ -0,0 +1,20 @@
+'''
+
+'''
+
+
+class Snake:
+    '''
+    
+    '''
+
+    def _init_(self, color, x:int, y:int, length: int)
+
+
+
+
+
+        self.x = x
+        self.y= y
+        self.length = 1
+        self.head_size
